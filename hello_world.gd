0988c6c5314a2766ed5eea32ebc81f0fd366279f
raindarle <160460783+raindarle@users.noreply.github.com>
2026-01-31 17:23:13 +0800

    Add files via upload

--- hello_world.gd
+++ hello_world.gd
@@ -1,14 +1,40 @@
 extends Label
 
-var fade_speed = 0.5
-var glitch_strength = 10
-var glitch_start_alpha = 0.85
+@export var speed: float = 210.0
+@export var shake_strength: float = 6.0
+@export var shake_duration: float = 0.1
+@export var right_limit: float = 600.0   # change based on window width
+@export var left_limit: float = 0.0
+
+var direction: int = 1
+var base_position: Vector2
+var shake_timer: float = 0.0
+
+func _ready():
+	base_position = position
 
 func _process(delta):
-	
-	if  modulate.a < 1:
-		modulate.a += fade_speed * delta
-		modulate.a = clamp(modulate.a, 0, 1)
+	# --- Normal Movement ---
+	position.x += speed * direction * delta
+
+	# --- Edge Collision ---
+	if position.x > right_limit:
+		direction = -1
+		start_shake()
+	elif position.x < left_limit:
+		direction = 1
+		start_shake()
+
+	# --- Shake Effect ---
+	if shake_timer > 0.0:
+		shake_timer -= delta
+		var offset = Vector2(
+			randf_range(-shake_strength, shake_strength),
+			randf_range(-shake_strength, shake_strength)
+		)
+		position = base_position + offset
 	else:
-		position.x += randf_range(-glitch_strength, glitch_strength)
-		position.y += randf_range(-glitch_strength, glitch_strength)
+		base_position = position  # update resting position
+
+func start_shake():
+	shake_timer = shake_duration
